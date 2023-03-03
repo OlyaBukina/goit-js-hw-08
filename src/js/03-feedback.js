2e054a0288fc2f3cdfbfdf63301ea22d6e98a4bd
OlyaBukina <katniss30051991@gmail.com>
2023-03-03 14:46:30 +0200

    Finished third task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('.feedback-form input'),
+  message: document.querySelector('.feedback-form textarea'),
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onTextInput, 500));
+populateTextArea();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const formElements = e.currentTarget.elements;
+  const user = {
+    email: formElements.email.value,
+    message: formElements.message.value,
+  };
+
+  if (user.email === '' || user.message === '') {
+    alert('All form fields must be completed!');
+  } else {
+    e.currentTarget.reset();
+    console.log(user);
+  }
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onTextInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+function populateTextArea() {
+  const parsedForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (parsedForm.email) {
+    refs.email.value = parsedForm.email;
+  }
+
+  if (parsedForm.message) {
+    refs.message.value = parsedForm.message;
+  }
+}
