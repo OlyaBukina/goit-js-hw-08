846b607ca521bdadf1200b9afa2730f4972d3681
OlyaBukina <katniss30051991@gmail.com>
2023-03-03 13:11:28 +0200

    Finished second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+function onPlay(timeupdate) {
+  localStorage.setItem(STORAGE_KEY, timeupdate.seconds);
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
