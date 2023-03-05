9c6ef09886fe2fbcbdeae5c465c5cd47178d451b
OlyaBukina <katniss30051991@gmail.com>
2023-03-05 22:35:04 +0200

    Fixed third tast

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,44 +5,30 @@ const refs = {
   email: document.querySelector('.feedback-form input'),
   message: document.querySelector('.feedback-form textarea'),
 };
-
 const STORAGE_KEY = 'feedback-form-state';
-const formData = {};
 
-refs.form.addEventListener('submit', onFormSubmit);
-refs.form.addEventListener('input', throttle(onTextInput, 500));
-populateTextArea();
-
-function onFormSubmit(e) {
-  e.preventDefault();
-
-  const formElements = e.currentTarget.elements;
-  const user = {
-    email: formElements.email.value,
-    message: formElements.message.value,
-  };
-
-  if (user.email === '' || user.message === '') {
-    alert('All form fields must be completed!');
-  } else {
-    e.currentTarget.reset();
-    console.log(user);
-  }
-  localStorage.removeItem(STORAGE_KEY);
+let formData;
+function loadFormData() {
+  formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) ?? {};
+  refs.email.value = formData?.email ?? '';
+  refs.message.value = formData?.message ?? '';
 }
-
+loadFormData();
 function onTextInput(e) {
   formData[e.target.name] = e.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
-function populateTextArea() {
-  const parsedForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
+refs.form.addEventListener('input', throttle(onTextInput, 500));
 
-  if (parsedForm.email) {
-    refs.email.value = parsedForm.email;
-  }
+function onFormSubmit(e) {
+  e.preventDefault();
 
-  if (parsedForm.message) {
-    refs.message.value = parsedForm.message;
+  if (!formData.email || !formData.message) {
+    alert('All form fields must be completed!');
+  } else {
+    console.log(formData);
+    localStorage.removeItem(STORAGE_KEY);
+    loadFormData();
   }
 }
+refs.form.addEventListener('submit', onFormSubmit);
